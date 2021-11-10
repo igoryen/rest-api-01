7690997fdc5b8c7303d556c17e13c2381d3c595e
igoryen <igor.yentaltsev@gmail.com>
2021-11-09 22:46:49 -0500

    start fleshing out routes

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,4 +1,29 @@
 const express = require('express');
 const router = express.Router();
 
+// a route for getting all
+router.get('/', (req, res) => {
+
+});
+
+// a route for getting one
+router.get('/:id', (req, res) => {
+
+});
+
+// a route for creating one
+router.post('/', (req, res) => {
+
+});
+
+// a route for updating one
+router.patch('/:id', (req, res) => {
+
+});
+
+// a route for deleting one
+router.delete('/:id', (req, res) => {
+
+});
+
 module.exports = router;
